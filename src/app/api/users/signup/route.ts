6e401bf9961b7fc25bf6a728386cd2824514d944
Yash Darbar <darbaryash44@gmail.com>
2024-02-18 03:41:22 +0530

    another issuse is resolve

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -10,7 +10,6 @@ export async function POST(req:any) {
         const reqBody = await req.json();
         const { username, email, password } = reqBody;
         console.log(reqBody);
-
         //check if user already exists PS:it should be in await or else it'll give u a querry
         const user = await User.findOne({email})
         if (user) {
@@ -23,15 +22,21 @@ export async function POST(req:any) {
 
         const newUser = new User({username, email, password: hashPassword})
 
-        const savedUser = newUser.saved()
-        console.log(savedUser);
+        // const savedUser = newUser.saved()
+        // console.log(savedUser);
 
-        return NextResponse.json({message:"user is successfully created!",
-        success: true,
-        savedUser})
+        // return NextResponse.json({message:"user is successfully created!",
+        // success: true,
+        // savedUser})
+        return NextResponse.json({
+            message: "user is successfully created!",
+            success: true,
+        });
 
 
     } catch (error: any) {
+        console.log(error);
         return NextResponse.json({ error: error.message }, { status: 500 });
+
     }
 }
