158e2a091ce07e0a328aead1b7af6bd04d54fa89
Yash Darbar <darbaryash44@gmail.com>
2024-02-19 01:03:40 +0530

    commit

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -3,39 +3,40 @@ import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import User from "@/models/userModels.js";
 
-connect()
+connect();
 
-export async function POST(req:any) {
+export async function POST(req: NextRequest) {
     try {
         const reqBody = await req.json();
         const { username, email, password } = reqBody;
         console.log(reqBody);
-        //check if user already exists PS:it should be in await or else it'll give u a querry
-        const user = await User.findOne({email})
+        //check if user already exists PS:it should be in await or else it'll give u a querry/ bcos it's a data base call
+        const user = await User.findOne({ email });
         if (user) {
-            return NextResponse.json({error: 'User already exists'},{ status: 500})
+            return NextResponse.json(
+                { error: "User already exists" },
+                { status: 500 }
+            );
         }
 
         //hash the password
-        const salt = await bcryptjs.genSalt(10)
-        const hashPassword = await bcryptjs.hash(password, salt)
+        const salt = await bcryptjs.genSalt(10);
+        const hashPassword = await bcryptjs.hash(password, salt);
 
-        const newUser = new User({username, email, password: hashPassword})
+        const newUser = new User({ username, email, password: hashPassword });
 
-        // const savedUser = newUser.saved()
-        // console.log(savedUser);
+        const savedUser = newUser.save()
+        console.log(savedUser);
 
-        // return NextResponse.json({message:"user is successfully created!",
-        // success: true,
-        // savedUser})
-        return NextResponse.json({
-            message: "user is successfully created!",
-            success: true,
-        });
-
+        return NextResponse.json({message:"user is successfully created!",
+        success: true,
+        savedUser})
+        // return NextResponse.json({
+        //     message: "user is successfully created!",
+        //     success: true,
+        // });
     } catch (error: any) {
         console.log(error);
         return NextResponse.json({ error: error.message }, { status: 500 });
-
     }
 }
